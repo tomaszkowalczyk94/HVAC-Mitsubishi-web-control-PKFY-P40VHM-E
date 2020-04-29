5f69daac684c3fcc95eb72aa1058467a9080701d
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2020-04-29 18:51:57 +0200

    arduino code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,30 +1,59 @@
 #include <SoftwareSerial.h>
-
+#include <IRremote.h>
 SoftwareSerial hc06(12,13);
 
+int MAX_SIGNAL_DELAY = 300;
+int KHZ = 38;
 
 void setup(){
   //Initialize Serial Monitor
   Serial.begin(9600);
-  Serial.println("ENTER AT Commands:");
   //Initialize Bluetooth Serial Port
   hc06.begin(9600);
 }
 
+
+
+
+unsigned long lastByteTime = 0;
+boolean signalFlushed = true;
+long numberOfNextByteToWrite = 0;
+
+
+byte buffor[300];
+
 void loop(){
   //Write data from HC06 to Serial Monitor
   if (hc06.available()){
-    Serial.write(hc06.read());
+    byte readedData = hc06.read();
+    
+    Serial.write(readedData);
+    Serial.write("\n");
+
+    buffor[numberOfNextByteToWrite] = readedData;
+    
+    lastByteTime = millis();
+    signalFlushed = false;
+  } else {
+    if(lastByteTime + MAX_SIGNAL_DELAY <  millis() && !signalFlushed) {
+      flush();
+      signalFlushed = true;
+      numberOfNextByteToWrite = 0;
+    }
   }
+}
+
+void flush() {
+  Serial.write("MAMY TO");
+
+  unsigned int irSignal[] = {9000, 4500, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 39416, 9000, 2210, 560}; //AnalysIR Batch Export (IRremote) - RAW
   
-  //Write from Serial Monitor to HC06
-  if (Serial.available()){
-    hc06.write(Serial.read());
-  }  
+  irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), KHZ); //Note the approach used to automatically calculate the size of the array.
+  
 }
 
 
-//#include <IRremote.h>
+
 
 //void setup() {
 //  // put your setup code here, to run once:
@@ -35,9 +64,7 @@ void loop(){
 //
 //void loop() {
 //  int khz = 38; // 38kHz carrier frequency for the NEC protocol
-//  unsigned int irSignal[] = {9000, 4500, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 39416, 9000, 2210, 560}; //AnalysIR Batch Export (IRremote) - RAW
-//  
-//  irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz); //Note the approach used to automatically calculate the size of the array.
+
 //
 //  delay(10000); 
 //}
