51477ccb545d6045b45e01926b2b86a562d43365
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2020-05-03 19:50:02 +0200

    arduino code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -10,7 +10,7 @@ int KHZ = 38;
 unsigned long lastByteTime = 0;
 boolean signalFlushed = true;
 long countOfBufforBytes = 0;
-byte buffor[560];
+byte buffor[40];
 
 void setup(){
   //Initialize Serial Monitor
@@ -43,53 +43,16 @@ void readBtData() {
 
 
 void flush() {
-
-  static unsigned int dataToSend[280];
-  readDataToSendFromBufor(dataToSend);
+  static unsigned int irRawSignal[300];
+  prepareRawSignal(irRawSignal, buffor);
   
-  
-
 //  unsigned int irSignal[] = {9000, 4500, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 39416, 9000, 2210, 560}; //AnalysIR Batch Export (IRremote) - RAW
 //  
 //  irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), KHZ); //Note the approach used to automatically calculate the size of the array.
   
 }
 
-/**
- * return count of array
- */
-int readDataToSendFromBufor(unsigned int *dataToSend) {
-
-  Serial.println("=== XX: ===");
-  Serial.println(buffor[0], HEX);
-  Serial.println(buffor[1], HEX);
+void prepareRawSignal(unsigned int *irRawSignal, byte *data) {
   
-  volatile int iDataToSend = 0;
-  volatile unsigned int valueToSave = 0;
   
-  Serial.println("countOfBufforBytes:");
-  Serial.println(countOfBufforBytes);
-  
-  for(int i; i<countOfBufforBytes;i++) {
-
-    if(i%2 == 0) {
-      Serial.println("i%2 == 0:");
-      valueToSave = 0;
-      valueToSave = buffor[i];
-      Serial.println("valueToSave");
-      Serial.println(valueToSave, HEX);
-    } else {
-      //dataToSend[iDataToSend] = valueToSave | (buffor[i]<<8);
-      //dataToSend[iDataToSend] = (buffor[i]);
-      Serial.println("else:");
-      Serial.println(valueToSave, HEX);
-
-      //dataToSend[iDataToSend] =  (valueToSave<<8) | buffor[i];      
-     dataToSend[iDataToSend] = 2;
-      iDataToSend++;
-    }
-  }
-  
-//  Serial.println("readed raw data:");
-//  Serial.println(dataToSend[0], HEX);
 }
